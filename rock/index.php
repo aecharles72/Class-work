c8f1f4c31591522cbd8c376543090a64fe2f16ca
aecharles72 <88692424+aecharles72@users.noreply.github.com>
2021-10-06 23:36:02 +0000

    Add files via upload

---
+++ rock/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Andrew Charles 3e6af247 Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+</div>
+</body>
+</html>
